0052f407eac2499b7cb119ee5a2f5db2b61b15ec
Leonardo Borges <laion459@gmail.com>
2022-05-28 10:24:24 -0300

    Initial commit

---
+++ app/src/main/java/programacao/mobile/atividade01chatparteii/MainActivity.java
@@ -0,0 +1,14 @@
+package programacao.mobile.atividade01chatparteii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
